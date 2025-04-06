c7b3c41bd26cae1ec1a60caea3d81f4c1dda04d5
MirroXDev <vitaliy12392@gmail.com>
2025-04-06 14:41:39 +0300

    start bot

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,7 +1,22 @@
-import { Update } from "nestjs-telegraf";
-import { Telegraf } from "telegraf";
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
+import { Scenes, Telegraf } from 'telegraf';
+
+type Context = Scenes.SceneContext;
 
 @Update()
-export class TelegramService extends Telegraf {
+export class TelegramService extends Telegraf<Context> {
+  @Start()
+  onStart(@Ctx() ctx: Context) {
+    ctx.replyWithHTML(`
+    <b>Привет, ${ctx.from?.username}</b>
+    Это бот для общения с ChatGPT.
+    Задайте любой вопрос и он ответит на него.
+    `);
+  }
 
+  @On('text')
+  onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
+      
+      
+  }
 }
